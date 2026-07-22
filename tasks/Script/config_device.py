38c9c05505ae0578266edb5e210a334871d132cb
xianren <1359965050@qq.com>
2026-07-22 21:06:24 +0800

    refactor: 优化多模块UI匹配与资源配置
    1.  更新全渠道包名配置并补充注释
    2.  调整MemoryScrolls、ActivityShikigami等模块的ROI匹配区域
    3.  替换绘卷模块的专用返回按钮为通用全局返回按钮
    4.  修复MuMu12模拟器进程状态检测逻辑
    5.  清理废弃的本地图片资源文件

--- tasks/Script/config_device.py
+++ tasks/Script/config_device.py
@@ -10,12 +10,15 @@ from module.logger import logger
 
 class PackageName(str, Enum):
     AUTO = 'auto'
-    NETEASE_ONMYOJI = 'com.netease.onmyoji.wyzymnqsd_cps'  # 网易自家的阴阳师
+    NETEASE_ONMYOJI = 'com.netease.onmyoji.wyzymnqsd_cps'  # 网易官方扫码版
     NETEASE_MI = 'com.netease.onmyoji.mi'  # 小米
-    NETEASE = 'com.netease.onmyoji'
-    NETEASE_HUAWEI = 'com.netease.onmyoji.huawei'
-    NETEASE_BILIBILI = 'com.netease.onmyoji.bili'
-    NETEASE_QQ = 'com.tencent.tmgp.yys.zqb'
+    NETEASE = 'com.netease.onmyoji'  # 网易官方非扫码版
+    NETEASE_HUAWEI = 'com.netease.onmyoji.huawei'  # 华为渠道版
+    NETEASE_BILIBILI = 'com.netease.onmyoji.bili'  #哔哩哔哩渠道服
+    NETEASE_VIVO = 'com.netease.onmyoji.vivo'  # vivo渠道服
+    NETEASE_M4399 = 'com.netease.onmyoji.m4399'  # 4399渠道服
+    NETEASE_NEARME = 'com.netease.onmyoji.nearme.gamecenter'  # OPPO渠道服
+    TENCENT_YYS = 'com.tencent.tmgp.yys.zqb'  # 应用宝渠道
 
 
 
